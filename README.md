806816c9d92f86492c778b5ca0952927ff1abe61
Bilal Durrani <bilal33@gmail.com>
2021-09-23 23:01:58 -0400

    better debugging

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ sudo apt install cmake
 ```bash
 apt install pkg-config
 
-apt-get install --yes --no-install-recommends build-essential cmake ninja-build libjpeg-dev libopenjp2-7-dev qtbase5-dev gobject-introspection libglib2.0-dev libgtk-3-dev libgirepository1.0-dev libnss3-dev ca-certificates libcurl4-nss-dev liblcms2-dev libboost-container-dev
+apt-get install --yes --no-install-recommends build-essential cmake ninja-build libjpeg-dev libopenjp2-7-dev qtbase5-dev gobject-introspection libglib2.0-dev libgtk-3-dev libgirepository1.0-dev libnss3-dev ca-certificates libcurl4-nss-dev liblcms2-dev libboost-container-dev libpoppler-private-dev libpoppler-cpp-dev
 ```
 
 Also need popper-dev.
--- wrapperLib/wrapper.cpp
+++ wrapperLib/wrapper.cpp
@@ -8,21 +8,23 @@
 
 #define UNUSED(x) (void)(x)
 
-EndOfLineKind textEOL = TextOutputDev::defaultEndOfLine();
+// EndOfLineKind textEOL = TextOutputDev::defaultEndOfLine();
 
 using namespace poppler;
+
 extern "C"
 {
   int test(const char *filePath)
   {
-    TextOutputDev *textOut;
-    bool physLayout = false;
-    bool fixedPitch = false;
-    bool rawOrder = false;
-    bool htmlMeta = false;
-    bool discardDiag = false;
+    // TextOutputDev *textOut;
+    // bool physLayout = false;
+    // bool fixedPitch = false;
+    // bool rawOrder = false;
+    // bool htmlMeta = false;
+    // bool discardDiag = false;
+    // double resolution = 72.0;
 
-    UNUSED(textOut);
+    // UNUSED(textOut);
     document *doc = document::load_from_file(filePath, "", "");
     if (!doc)
     {
@@ -31,13 +33,16 @@ extern "C"
 
     auto pageCount = doc->pages();
     UNUSED(pageCount);
-    textOut = new TextOutputDev("result.txt", physLayout, fixedPitch,
-                                rawOrder, htmlMeta, discardDiag);
-    if (textOut->isOk())
-    {
-      textOut->setTextEOL(textEOL);
-    }
-    delete textOut;
+    auto page = doc->create_page(0);
+    page->text();
+    // textOut = new TextOutputDev("result.txt", physLayout, fixedPitch,
+    //                             rawOrder, htmlMeta, discardDiag);
+    // if (textOut->isOk())
+    // {
+    //   textOut->setTextEOL(textEOL);
+
+    // }
+    // delete textOut;
     return 0;
   }
 
