5c91d9d3c08162296fb612926eaa17f87bc6bf6b
Bilal Durrani <bilal.durrani@xero.com>
2021-09-23 21:52:25 -0400

    link with poppler libs

--- wrapperLib/wrapper.cpp
+++ wrapperLib/wrapper.cpp
@@ -4,18 +4,40 @@
 #include <poppler-document.h>
 #include <poppler-page.h>
 #include <poppler-toc.h>
+#include <TextOutputDev.h>
+
+#define UNUSED(x) (void)(x)
+
+EndOfLineKind textEOL = TextOutputDev::defaultEndOfLine();
 
 using namespace poppler;
 extern "C"
 {
   int test(const char *filePath)
   {
+    TextOutputDev *textOut;
+    bool physLayout = false;
+    bool fixedPitch = false;
+    bool rawOrder = false;
+    bool htmlMeta = false;
+    bool discardDiag = false;
+
+    UNUSED(textOut);
     document *doc = document::load_from_file(filePath, "", "");
     if (!doc)
     {
       return 1;
     }
 
+    auto pageCount = doc->pages();
+    UNUSED(pageCount);
+    textOut = new TextOutputDev("result.txt", physLayout, fixedPitch,
+                                rawOrder, htmlMeta, discardDiag);
+    if (textOut->isOk())
+    {
+      textOut->setTextEOL(textEOL);
+    }
+    delete textOut;
     return 0;
   }
 
