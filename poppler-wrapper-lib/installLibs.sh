404834ca8b082351efb20681f659847f337aeb22
Bilal Durrani <bilal33@gmail.com>
2021-09-26 09:03:21 -0400

    re-org folders

--- poppler-wrapper-lib/installLibs.sh
+++ poppler-wrapper-lib/installLibs.sh
@@ -8,8 +8,8 @@ popd > /dev/null
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	mkdir -p ./dotnet/console-app/runtimes/linux-x64
 	echo "Copying linux-x64 runtime"
-	cp ./_install/lib/libwrapper.so ./dotnet/console-app/runtimes/linux-x64/
+	cp ./_install/lib/libwrapper.so ../dotnet/console-app/runtimes/linux-x64/
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	mkdir -p ./dotnet/console-app/runtimes/osx-x64
-	cp ./_install/lib/libwrapper.dylib ./dotnet/console-app/runtimes/osx-x64/
+	cp ./_install/lib/libwrapper.dylib ../dotnet/console-app/runtimes/osx-x64/
 fi
