ba5787fc68e2e025c88b818c00546c64b308c244
Bilal Durrani <bilal.durrani@xero.com>
2021-09-27 15:52:11 -0400

    added diffing unit tests

--- poppler-wrapper-lib/installLibs.sh
+++ poppler-wrapper-lib/installLibs.sh
@@ -6,11 +6,11 @@ cmake --install . --prefix ../_install --config Debug
 popd > /dev/null
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	mkdir -p ./dotnet/console-app/runtimes/linux-x64
 	echo "Copying linux-x64 runtime"
+	mkdir -p ../dotnet/console-app/runtimes/linux-x64
 	cp ./_install/lib/libwrapper.so ../dotnet/poppler-lib/runtimes/linux-x64/
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	echo "Copying mac os runtime"
-	mkdir -p ./dotnet/console-app/runtimes/osx-x64
+	mkdir -p ../dotnet/console-app/runtimes/osx-x64
 	cp ./_install/lib/libwrapper.dylib ../dotnet/poppler-lib/runtimes/osx-x64/
 fi
--- poppler-wrapper-lib/test-exec/main.cpp
+++ poppler-wrapper-lib/test-exec/main.cpp
@@ -2,36 +2,119 @@
 #include <cstdlib>
 #include "wrapper/wrapper.h"
 #include <sys/stat.h>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
-inline bool exists(const char *path)
+/**
+ * Cross platform path separator
+ **/
+const char kPathSeparator =
+#ifdef _WIN32
+    '\\';
+#else
+    '/';
+#endif
+
+const string TestFolderName = "test-assets";
+
+static inline bool exists(const char *path)
 {
   struct stat buffer;
   return (stat(path, &buffer) == 0);
+}
+
+static string BuildTestPdfPath(const string &pdfName)
+{
+  string path(".");
+  path += kPathSeparator;
+  path += TestFolderName;
+  path += kPathSeparator;
+  path += pdfName;
+  return path;
+}
+
+static string BuildGoldenDatasetPath(const string &pdfName)
+{
+  string path(".");
+  path += kPathSeparator;
+  path += TestFolderName;
+  path += kPathSeparator;
+  path += "golden-data";
+  path += kPathSeparator;
+  path += pdfName;
+  return path;
+}
+
+bool comparePdfText(const std::string &data, const std::string &goldenDataPath)
+{
+  std::ifstream goldenDataStream(goldenDataPath);
+  if (goldenDataStream.fail())
+  {
+    cerr << "Failed to read golden dataset from" << goldenDataPath << endl;
+    return false;
+  }
+
+  std::stringstream buffer;
+  buffer << goldenDataStream.rdbuf();
+
+  auto goldenData = buffer.str();
+  cout << "Data len: " << data.length() << endl
+       << " Golden data len: " << buffer.str().length();
+
+  return data.compare(goldenData) == 0;
 }
 
 int main(int argc, char *argv[])
 {
   if (argc == 1)
   {
-    cout << "No args found" << endl;
+    cerr << "Missing args" << endl;
     return 1;
   }
 
-  cout << argv[1] << endl;
+  const char *pdfName = argv[1];
 
-  if (!exists(argv[1]))
+  string testPdfPath = BuildTestPdfPath(pdfName);
+
+  if (!exists(testPdfPath.c_str()))
   {
-    cout << "File not found" << endl;
+    cerr << "File not found" << testPdfPath << endl;
+    return 1;
   }
 
-  // const char *status = test(argv[1]);
-  // cout << status << endl;
-  auto doc = create_new_document_from_file(argv[1]);
-  auto page = document_get_page(doc, 0);
-  const char *txt = page_get_text(page);
+  auto doc = create_new_document_from_file(testPdfPath.c_str());
+  auto pageCount = document_get_pagecount(doc);
 
-  cout << txt << endl;
-  std::free((void *)txt);
+  std::string documentTxt;
+  for (int i = 0; i < pageCount; i++)
+  {
+    auto page = document_get_page(doc, i);
+    const char *txt = page_get_text(page);
+
+    if (txt == nullptr)
+    {
+      cerr << "Output text was null" << endl;
+      return 1;
+    }
+    documentTxt.append(txt);
+    delete_text_buffer((void *)txt);
+  }
+
+  cout << documentTxt;
+
+  string txtFileName(pdfName);
+  txtFileName = txtFileName.substr(0, txtFileName.find_last_of('.')) + ".txt";
+
+  auto goldenDatasetPath = BuildGoldenDatasetPath(txtFileName);
+  bool isSame = comparePdfText(documentTxt, goldenDatasetPath);
+  cout << std::boolalpha << "PDFs match: " << isSame << endl;
+  if (!isSame)
+  {
+    cerr << "PDF text output does not match" << endl;
+    return 1;
+  }
+  return 0;
 }
