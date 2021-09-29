b646aa40a7cec03efe26f19f76cd0ddc00af8804
Bilal Durrani <bilal33@gmail.com>
2021-09-28 23:00:11 -0400

    add trim support

--- poppler-wrapper-lib/test-exec/main.cpp
+++ poppler-wrapper-lib/test-exec/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <cstdlib>
-#include "wrapper/wrapper.h"
 #include <sys/stat.h>
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <algorithm>
+#include "wrapper/wrapper.h"
 
 using namespace std;
 
@@ -51,6 +52,29 @@ static string BuildGoldenDatasetPath(const string &pdfName)
 inline unsigned int to_uint(char ch)
 {
   return static_cast<unsigned int>(static_cast<unsigned char>(ch));
+}
+
+// trim from start (in place)
+static inline void ltrim(std::string &s)
+{
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch)
+                                  { return !std::isspace(ch); }));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string &s)
+{
+  s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch)
+                       { return !std::isspace(ch); })
+              .base(),
+          s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string &s)
+{
+  ltrim(s);
+  rtrim(s);
 }
 
 bool comparePdfText(const std::string &data, const std::string &goldenDataPath)
